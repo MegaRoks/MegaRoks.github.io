c4d6e94532027e7bef958a136f620d6d0b4d9851
MegaRoks <MegaRoks@gmail.com>
2018-07-02 19:16:56 +0600

    Added information about myself and added the function of calculating the year

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+var day = 20;
+var month = 04;
+var year = 1994;
+var time = new Date();
+var age = (time.getFullYear() - year - ((time.getMonth() - --month || time.getDate() - day) < 0));
+console.log(age);
+var a = document.getElementById("age");
+a.innerText = age;
+
 function unmask(blog) {
 	for (var i = 0; i < 4; i++) {
 		if (blog === i) {
